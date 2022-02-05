7d3dc159792802f24ebbef5b7bbbc7b003538206
quocviettran <quocviettran95@gmail.com>
2022-02-05 18:55:41 +0100

    initial

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title> Instagram 2.0 by Quoc Viet Tran</title>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Feed  content */}
+
+      {/* Modal drop down */}
+
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const tailwindcss = require("tailwindcss");
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
